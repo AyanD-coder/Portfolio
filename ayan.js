a9c26391a88485038a3d752909ac045a61b9c7d8
Ayan Dutta <ad.ayandutta@gmail.com>
2026-03-15 00:33:05 +0530

    updated portfolio with new website

--- ayan.js
+++ ayan.js
@@ -11,8 +11,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const skillList = document.getElementById("skilllist");
   const bio = document.getElementById("bio");
   const img1= document.getElementById("img1");
+const githubprojects = document.querySelector(".github-projects");
 
-  
   function checkAndToggleVisibility() {
     if (aboutSection.classList.contains("show") || contactSection.classList.contains("show")) {
       img1.style.display = "none";
@@ -22,6 +22,28 @@ document.addEventListener("DOMContentLoaded", () => {
       bio.style.display = "block";
     }
   }
+
+  
+  async function loadRepos() {
+    try {
+      const response = await fetch("https://api.github.com/users/AyanD-coder/repos");
+      const data = await response.json();
+
+      let output = "<h3>My GitHub Projects</h3><ul>";
+      data.slice(0, 3).forEach(repo => {
+        output += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`;
+      });
+      output += "</ul>";
+
+      document.querySelector(".github-projects").innerHTML = output;
+    } catch (error) {
+      console.error("Error fetching repos:", error);
+    }
+  }
+
+  loadRepos();
+
+   
   
 
   contactBtn.addEventListener("click", () => {
@@ -74,22 +96,35 @@ document.addEventListener("DOMContentLoaded", () => {
     if (projectList.classList.contains("show")) {
       // If project list is already open, close it and show bio
       projectList.classList.remove("show");
+      githubprojects.classList.add("hide");
       bio.classList.remove("hide");
     } else {
       // Open project list, hide skill list and bio
       projectList.classList.add("show");
+      githubprojects.classList.remove("hide");
       skillList.classList.remove("show");
       skillList.classList.add("hide");
       bio.classList.add("hide");
     }
   });
-  
+
+  githubprojects.addEventListener("click", () => {
+    if (githubprojects.classList.contains("show")) {
+      githubprojects.classList.remove("show");
+    } else {
+      githubprojects.classList.add("show");
+    }
+  });
+
   skillBtn.addEventListener("click", () => {
-    if (skillList.classList.contains("show")) {
+    if (skillList.classList.contains("show") || githubprojects.classList.contains("show")
+    ) {
       // If skill list is already open, close it and show bio
       skillList.classList.remove("show");
       skillList.classList.add("hide");
       bio.classList.remove("hide");
+      githubprojects.classList.remove("show");
+      githubprojects.classList.add("hide");
      
     } else {
       // Open skill list, hide project list and bio
@@ -100,7 +135,4 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
   
-
-
 });
-
